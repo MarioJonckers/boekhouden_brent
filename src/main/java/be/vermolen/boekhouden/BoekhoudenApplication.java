e18f734f3fa25d99413459a685f51d5653b10754
Brent Vermolen <brentvermolen@hotmail.be>
2022-05-07 10:35:32 +0200

    Init commit

---
+++ src/main/java/be/vermolen/boekhouden/BoekhoudenApplication.java
@@ -0,0 +1,13 @@
+package be.vermolen.boekhouden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoekhoudenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoekhoudenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/vermolen/boekhouden/BoekhoudenApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vermolen.boekhouden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoekhoudenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

bb053ffa0b7911459c9d16b5b704d53146d3ae2f
Brent Vermolen <brentvermolen@hotmail.be>
2022-05-09 10:03:26 +0200

    FE: Add sidebar

--- src/main/resources/frontend/boekhouden-app/src/app/app-routing.module.ts
+++ src/main/resources/frontend/boekhouden-app/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: MainComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      relativeLinkResolution: 'legacy',
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/main/resources/frontend/boekhouden-app/src/app/app.component.ts
+++ src/main/resources/frontend/boekhouden-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Modes } from 'angular-sidebar-menu';
 import { GeneralService } from './services/general.service';
 
 @Component({
@@ -8,12 +9,62 @@ import { GeneralService } from './services/general.service';
 })
 export class AppComponent {
   title = 'boekhouden-app';
+  mode = Modes.EXPANDED;
+  search = '';
 
-  constructor(private generalService: GeneralService) {}
+  menu = [
+    {
+      header: 'Documentation',
+    },
+    {
+      label: 'Get Started',
+      route: '',
+      iconClasses: 'fa fa-rocket',
+    },
+    {
+      label: 'Multilevel',
+      iconClasses: 'fa fa-share',
+      children: [
+        {
+          label: 'Configuration 1',
+          url: '//google.com',
+          badges: [
+            {
+              label: '1',
+              classes: 'badge--red',
+            },
+          ],
+        },
+        {
+          header: 'Separator',
+        },
+        {
+          label: 'Configuration 2',
+          children: [
+            {
+              label: 'Configuration 1',
+              url: '//google.com',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: 'Badges',
+      iconClasses: 'fa fa-star',
+      url: '//google.com',
+      badges: [
+        {
+          label: 'new',
+          classes: 'badge--red',
+        },
+        {
+          label: '1',
+          classes: 'badge--blue',
+        },
+      ],
+    },
+  ];
 
-  ngOnInit(): void {
-    this.generalService.getTest().subscribe((data: any) => {
-      console.log(data);
-    });
-  }
+  constructor() {}
 }
